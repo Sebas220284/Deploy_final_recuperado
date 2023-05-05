46cd699665fc65b56a3405cd0d022de62f3f84bb
Sebastian Vazquez <sebastianvazquez928@gmail.com>
2023-05-04 22:43:55 -0600

    server

--- cliente/src/server/server.js
+++ cliente/src/server/server.js
@@ -21,4 +21,4 @@ io.on("connection", (socket) => {
 
 // Inicia el servidor
 //const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${port}`));
+server.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`));
